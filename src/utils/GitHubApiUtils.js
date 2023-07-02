4e1df0954ce9d0527bd5e0c933b833ded66d821d
Thiago Ferreira <thiagodnf@gmail.com>
2023-07-02 19:37:56 -0400

    added the github api

---
+++ src/utils/GitHubApiUtils.js
@@ -0,0 +1,51 @@
+const { Octokit } = require("@octokit/rest");
+
+module.exports = class GitHubApiUtils {
+
+    static async isValidRepository(str) {
+
+        if (!str || str.trim() === "") {
+            throw new Error("Repository should not be blank");
+        }
+
+        return /(.+)\/(.+)/gm.test(str);
+    }
+
+    static async getRepository() {
+
+        const repository = process.env["GITHUB_REPOSITORY"];
+
+        if (!GitHubApiUtils.isValidRepository(repository)) {
+            throw new Error(`${repository} is invalid`);
+        }
+
+        const [owner, repo] = repository.split("/");
+
+        let headers = {
+            accept: "application/vnd.github.v3+json",
+            authorization: `token ${process.env["GITHUB_TOKEN"]}`
+        };
+
+        return { owner, repo, headers };
+    }
+
+    static async getRelease() {
+
+        const octokit = new Octokit();
+
+        const repoInfo = await GitHubApiUtils.getRepository();
+
+        let releases = await octokit.rest.repos.listReleases(repoInfo);
+
+        releases = releases.data;
+
+        let latestRelease = "";
+
+        if (releases.length) {
+            latestRelease = releases[0].name;
+            latestRelease = latestRelease.trim().replace(/^v?(.*)$/gm, "$1");
+        }
+
+        return latestRelease;
+    }
+}
